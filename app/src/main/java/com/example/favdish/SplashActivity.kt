6ce396c04821141eb4611792d2755bcbbd406bc5
eavdic <emir.avdic@asseco-see.ba>
2022-12-12 13:58:02 +0100

    make splash fullscreen

--- app/src/main/java/com/example/favdish/SplashActivity.kt
+++ app/src/main/java/com/example/favdish/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.example.favdish
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.example.favdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -11,5 +14,15 @@ class SplashActivity : AppCompatActivity() {
 
         val splashActivity: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashActivity.root)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
 }
