387167e96aaac9fd055abf097e3c033da5cf5fe2
eavdic <emir.avdic@asseco-see.ba>
2022-12-15 16:44:42 +0100

    feat: implement custom dialog for camera and gallery selection

--- app/src/main/java/com/example/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/favdish/view/activities/AddUpdateDishActivity.kt
@@ -1,11 +1,13 @@
 package com.example.favdish.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.favdish.R
 import com.example.favdish.databinding.ActivityAddUpdateDishBinding
+import com.example.favdish.databinding.DialogCustomImageSelectionBinding
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mBinding: ActivityAddUpdateDishBinding
@@ -30,11 +32,26 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         if (v != null) {
             when(v.id) {
                 R.id.iv_add_dish_image -> {
-                    Toast.makeText(this, "You have clicked the ImageView",
-                    Toast.LENGTH_SHORT).show()
+                    customImageSelectionDialog()
                     return
                 }
             }
         }
+    }
+
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this)
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+        binding.tvCamera.setOnClickListener {
+            Toast.makeText(this, "Camera clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        binding.tvGallery.setOnClickListener {
+            Toast.makeText(this, "Gallery clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
